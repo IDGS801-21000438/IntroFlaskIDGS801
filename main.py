7755dfd201bcf13629593e87133c16339bd136e1
InaliQ <21000438@alumnos.utleon.edu.mx>
2024-01-22 18:20:04 -0600

    EUHM 22 ENERO

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask (__name__)
 
 
 @app.route("/")
 def index():
-    return "Hola Mundo"
+    return render_template("index.html")
 
 
 @app.route("/hola")
@@ -16,14 +16,25 @@ def hola():
 def saludo():
     return "<h1>Salidos desde Saludo</h1>"
 
-@app.route("/clientes")
-def cliente():
-    return "<h1>Salidos desde Clientes</h1>"
+@app.route("/nombre/<string:nombre>")
+def nombre(nombre):
+    return "Hola: "+nombre
 
-@app.route("/empleados")
-def empleado():
-    return "<h1>Salidos desde Empleados</h1>"
+@app.route("/numero/<string:numero>")
+def numero(numero):
+    return "Este es el numero:  " + str(numero)
+
+@app.route("/user/<string:id>/<string:nom>")
+def user(id,nom):
+    return "Bienvenido Usuario" + nom + " " + id
+
+@app.route("/suma/<string:num1>/<string:num2>")
+def suma(num1,num2):
+    total = int(num1) + int(num2)
+    return "La suma de" + str(num1) + " " + str(num2) + " es igual a: " + str(total) 
+
+
 
 
 if __name__ =="__main__":
-    app.run()
+    app.run(debug=True)
